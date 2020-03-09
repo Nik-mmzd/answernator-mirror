1f742cd62e947905828fda08bbe54d9d0d8bd08e
McModder <mcmodder@mcmodder.ru>
2020-03-09 19:07:18 +0300

    config fixes

--- Answernator/src/main/kotlin/pw/modder/answernator/utils/Config.kt
+++ Answernator/src/main/kotlin/pw/modder/answernator/utils/Config.kt
@@ -1,22 +1,20 @@
 package pw.modder.answernator.utils
 
-import kotlinx.serialization.Required
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.Transient
 import kotlinx.serialization.UnstableDefault
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonConfiguration
 import java.io.File
 import java.util.*
 
 @Serializable
 open class Config(
-    @Required val token: String = "token-here",
-    @Required val lang: String = "ru",
-    @Required val prefix: Char = '!',
-    @Required val author: String = "135017849604276224",
-    @Required val langs: List<String> = listOf("ru", "en"),
-    @Required val defaultStatus: String = "\$help"
+    val token: String = "token-here",
+    val lang: String = "ru",
+    val prefix: Char = '!',
+    val author: String = "135017849604276224",
+    val langs: List<String> = listOf("ru", "en"),
+    val defaultStatus: String = "\$help"
 ) {
     @Transient
     val locale = Locale(lang)
