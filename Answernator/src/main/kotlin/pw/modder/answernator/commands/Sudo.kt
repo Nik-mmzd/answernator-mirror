bb78f8035007597cf72a3da79c67d90ad3221796
McModder <mcmodder@mcmodder.ru>
2020-03-28 05:35:16 +0300

    sudo

---
+++ Answernator/src/main/kotlin/pw/modder/answernator/commands/Sudo.kt
@@ -0,0 +1,36 @@
+package pw.modder.answernator.commands
+
+import com.jessecorbett.diskord.api.model.Message
+import com.jessecorbett.diskord.dsl.Bot
+import com.jessecorbett.diskord.dsl.CombinedMessageEmbed
+import com.jessecorbett.diskord.util.words
+import kotlinx.serialization.UnstableDefault
+import pw.modder.answernator.utils.Command
+import pw.modder.answernator.utils.CommandList
+import pw.modder.answernator.utils.extensions.removeGraves
+import java.util.*
+
+@UnstableDefault
+class Sudo: Command {
+    override val name = "sudo"
+    override val cmdType = Command.CommandGroup.DEBUG
+    override val userGroup = Command.UserGroup.OWNER
+    override val channels = EnumSet.of(Command.ChannelTypes.GUILD)
+    override fun getDescription(locale: Locale): String? {
+        return "Run command as guild admin"
+    }
+
+    override fun getHelp(locale: Locale): String? {
+        return "Usage: `sudo [command] [command params]`"
+    }
+
+    override suspend fun action(bot: Bot, message: Message, locale: Locale): CombinedMessageEmbed {
+        if (message.words.size < 2) textMessage("No command specified.\n" + getHelp(locale))
+        CommandList.commands.singleOrNull {
+            it.name == message.words[1] && Command.ChannelTypes.GUILD in it.channels
+        }?.run {
+            return action(bot, message.copy(content = message.words.drop(1).joinToString(" ")), locale)
+        } ?: return textMessage("Command `${message.words[1].removeGraves()}` not found")
+    }
+
+}
