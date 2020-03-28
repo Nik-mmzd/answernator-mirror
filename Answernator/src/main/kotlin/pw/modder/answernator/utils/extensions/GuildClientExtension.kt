7a35044d8845cef63e7e0f9682dcc64cba0c7701
McModder <mcmodder@mcmodder.ru>
2020-03-28 05:52:43 +0300

    suppress warning

--- Answernator/src/main/kotlin/pw/modder/answernator/utils/extensions/GuildClientExtension.kt
+++ Answernator/src/main/kotlin/pw/modder/answernator/utils/extensions/GuildClientExtension.kt
@@ -4,5 +4,6 @@ import com.jessecorbett.diskord.api.rest.client.GuildClient
 import com.jessecorbett.diskord.util.DiskordInternals
 import pw.modder.answernator.utils.auditLog.AuditLog
 
+@Suppress("UNUSED_PARAMETER")
 @UseExperimental(DiskordInternals::class)
 suspend fun GuildClient.getAuditLog(useCustom: Boolean = true) = getRequest("/guilds/$guildId/audit-logs", AuditLog.serializer())
