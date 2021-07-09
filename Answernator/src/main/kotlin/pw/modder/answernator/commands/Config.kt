6fbaddcbae47f4d745fce51e0a002ebb4538ef7a
McModder <mcmodder@mcmodder.ru>
2021-07-09 22:30:03 +0300

    todo: config get; config blacklist get; config locale files

--- Answernator/src/main/kotlin/pw/modder/answernator/commands/Config.kt
+++ Answernator/src/main/kotlin/pw/modder/answernator/commands/Config.kt
@@ -3,17 +3,18 @@ package pw.modder.answernator.commands
 import com.jessecorbett.diskord.api.model.Message
 import com.jessecorbett.diskord.dsl.Bot
 import com.jessecorbett.diskord.dsl.CombinedMessageEmbed
-import com.jessecorbett.diskord.dsl.field
-import com.jessecorbett.diskord.util.ClientStore
-import com.jessecorbett.diskord.util.mention
+import com.jessecorbett.diskord.util.toRoleMention
 import com.jessecorbett.diskord.util.words
+import org.jetbrains.exposed.sql.transactions.transaction
+import pw.modder.answernator.cache.GuildCache.getCached
 import pw.modder.answernator.db.Db
-import pw.modder.answernator.db.GuildConfig
+import pw.modder.answernator.db.guild.BlacklistedCommand
+import pw.modder.answernator.db.guild.Features
 import pw.modder.answernator.utils.Command
+import pw.modder.answernator.utils.CommandList
 import pw.modder.answernator.utils.Globals
 import pw.modder.answernator.utils.LocalizedCommand
-import pw.modder.answernator.utils.extensions.toChannelMention
-import pw.modder.answernator.utils.extensions.toRoleMention
+import pw.modder.answernator.utils.extensions.channelsIdsMentioned
 import pw.modder.answernator.utils.locale.CommandLocaleBundle
 import java.util.*
 import com.jessecorbett.diskord.dsl.message as dslmessage
@@ -26,146 +27,152 @@ class Config: LocalizedCommand {
     override val cmdType = Command.CommandGroup.ADMIN
 
     override suspend fun action(bot: Bot, message: Message, texts: CommandLocaleBundle): CombinedMessageEmbed {
-        val guildClient = message.guildId?.run { bot.clientStore.guilds[this] } ?: return texts.getString("noguild").toMessage()
-        val guild = guildClient.get()
-        val config = Db.guilds.get(guild.id)
+        val guildClient = bot.clientStore.guilds[message.guildId ?: return texts.getString("noguild").toMessage()]
+        val guild = guildClient.getCached()
+        val config = Db.getConfig(guild.id)
 
-        return when(message.words.getOrNull(1)?.toLowerCase()) {
-            "get" -> {
+        if (message.words.isEmpty())
+            return texts.getErrorString().toMessage()
+
+        return when(message.words[1].toLowerCase()) {
+            "get", "show" -> {
                 dslmessage {
-                    title = guild.name
-                    description = texts.getString("description")
-
-                    field(texts.getString("lang"), config.lang, false)
-                    field(texts.getString("lang.available"), Globals.config.langs.joinToString(separator = ", ") { "`$it`" }, false)
-                    field(texts.getString("greeter"), texts.getString("greeter.${config.greetNewUsers}"), false)
-                    field(texts.getString("greeting"), String.format(config.greetingText, "%user%", "%guild%"), false)
-                    field(texts.getString("greeting.help"), texts.getString("greeting.help.value"), false)
-                    field(texts.getString("greeting.channel"), getGreetingsChannelName(bot.clientStore, config, texts), false)
-                    field(texts.getString("muterole"), config.muteRole.toRoleMention().ifEmpty { texts.getString("role.notset") }, true)
-                    field(texts.getString("defrole"), config.defaultRole.toRoleMention().ifEmpty { texts.getString("role.notset") }, true)
-                    field(texts.getString("antispam"), texts.getString("antispam.enabled.${config.antiSpam}"), true)
+                    TODO()
                 }
             }
-            "set" -> {
+            "lang" -> {
+                if (message.words.getOrNull(2) !in Globals.config.langs)
+                    return texts.formatString("locale.notfound", message.words[2]).toMessage()
+
+                transaction { config.lang = message.words[2] }
+                return texts.formatString("locale.updated", message.words[2]).toMessage()
+
+            }
+            "prefix" -> {
+                if (message.words.getOrNull(2)?.length != 1)
+                    return texts.formatString("prefix.incorrect", message.words[2]).toMessage()
+
+                transaction { config.cmdPrefix = message.words[2].toCharArray().first() }
+                texts.formatString("prefix.updated", message.words[2]).toMessage()
+            }
+            "greeting", "greet" -> {
                 when(message.words.getOrNull(2)) {
-                    "lang" -> {
-                        if (message.words[3] in Globals.config.langs) {
-                            Db.updateGuildConfig(guild.id) {
-                                it[lang] = message.words[3]
-                            }
-                            texts.formatString("locale.updated", message.words[3]).toMessage()
-                        } else texts.formatString("locale.notfound", message.words[3]).toMessage()
+                    "set" -> {
+                        transaction {
+                            config.greeting = message.words.drop(3)
+                                .joinToString(separator = " ")
+                                .replace("%user%", "%1\$s")
+                                .replace("%guild%", "%2\$s")
+                        }
+                        texts.getString("greeting.set").toMessage()
                     }
-                    "greet" -> {
-                        when(message.words.getOrNull(3)) {
-                            "set" -> {
-                                Db.updateGuildConfig(guild.id) {
-                                    it[greetingText] = message.words.drop(4)
-                                        .joinToString(separator = " ")
-                                        .replace("%user%", "%1\$s")
-                                        .replace("%guild%", "%2\$s")
-                                }
-                                texts.getString("greeting.applied").toMessage()
-                            }
-                            "enable" -> {
-                                Db.updateGuildConfig(guild.id) {
-                                    it[greetNewUsers] = true
-                                }
-                                texts.getString("greeting.enabled").toMessage()
-                            }
-                            "disable" -> {
-                                Db.updateGuildConfig(guild.id) {
-                                    it[greetNewUsers] = false
-                                }
-                                texts.getString("greeting.disabled").toMessage()
-                            }
-                            "channel" -> {
-                                val channel = try {
-                                    bot.clientStore.channels[extractChannelId(message.words[4])]
-                                } catch (_: Exception) {
-                                    return texts.getErrorString().toMessage()
-                                }
-
-                                Db.updateGuildConfig(guild.id) {
-                                    it[greetingsChannel] = channel.channelId
-                                }
-                                texts.formatString("greeting.channelset", channel.get().mention).toMessage()
-                            }
-                            else -> texts.getErrorString().toMessage()
+                    "enable" -> {
+                        transaction {
+                            config.enable(Features.GREETING)
                         }
+                        texts.getString("greeting.enabled").toMessage()
                     }
-                    "defrole" -> {
-                        if (message.words.getOrNull(3).equals("remove", true)) {
-                            Db.updateGuildConfig(guild.id) {
-                                it[defaultRole] = ""
-                            }
-                            return texts.getString("defrole.removed").toMessage()
+                    "disable" -> {
+                        transaction {
+                            config.disable(Features.GREETING)
                         }
-
-                        val role = message.rolesIdsMentioned.singleOrNull()
-                            ?: return texts.getErrorString().toMessage()
-
-                        Db.updateGuildConfig(guild.id) {
-                            it[defaultRole] = role
-                        }
-                        return texts.formatString("defrole.set", role.toRoleMention()).toMessage()
+                        texts.getString("greeting.disabled").toMessage()
                     }
-                    "muterole" -> {
-                        if (message.words.getOrNull(3).equals("remove", true)) {
-                            Db.updateGuildConfig(guild.id) {
-                                it[muteRole] = ""
-                            }
-                            return texts.getString("muterole.removed").toMessage()
-                        }
+                    "channel" -> {
+                        if (message.channelsIdsMentioned.size != 1)
+                            return texts.getString("greeting.channel.none").toMessage()
 
-                        val role = message.rolesIdsMentioned.singleOrNull()
-                            ?: return texts.getErrorString().toMessage()
-
-                        Db.updateGuildConfig(guild.id) {
-                            it[muteRole] = role
-                        }
-                        return texts.formatString("muterole.set", role.toRoleMention()).toMessage()
-                    }
-                    "antispam" -> when(message.words.getOrNull(3)) {
-                        "enable" -> {
-                            Db.updateGuildConfig(guild.id) {
-                                it[antiSpam] = true
-                            }
-                            return texts.getString("antispam.enabled").toMessage()
-                        }
-                        "disable" -> {
-                            Db.updateGuildConfig(guild.id) {
-                                it[antiSpam] = false
-                            }
-                            return texts.getString("antispam.disabled").toMessage()
-                        }
-                        else -> texts.getErrorString().toMessage()
+                        transaction { config.greetingChannel = message.rolesIdsMentioned.first() }
+                        texts.formatString("greeting.channel.set", message.rolesIdsMentioned.first().toRoleMention()).toMessage()
                     }
                     else -> texts.getErrorString().toMessage()
                 }
             }
-            "blacklist" -> TODO()
+            "defrole" -> {
+                when(message.words.getOrNull(2)) {
+                    "enable" -> {
+                        if (config.defaultRole == null)
+                            return texts.getString("defrole.missing").toMessage()
+
+                        transaction {
+                            config.enable(Features.DEFAULT_ROLE)
+                        }
+                        texts.getString("defrole.enabled").toMessage()
+                    }
+                    "disable" -> {
+                        transaction {
+                            config.disable(Features.DEFAULT_ROLE)
+                        }
+                        texts.getString("defrole.disabled").toMessage()
+                    }
+                    "set" -> {
+                        if (message.rolesIdsMentioned.size != 1)
+                            return texts.getString("defrole.invalid").toMessage()
+
+                        transaction { config.defaultRole = message.rolesIdsMentioned.first() }
+                        texts.formatString("defrole.set", message.rolesIdsMentioned.first().toRoleMention()).toMessage()
+                    }
+                    "unset" -> {
+                        transaction {
+                            config.disable(Features.DEFAULT_ROLE)
+                            config.defaultRole = null
+                        }
+                        texts.getString("defrole.unset").toMessage()
+                    }
+                    else -> texts.getErrorString().toMessage()
+                }
+            }
+            "muterole" -> {
+                when(message.words.getOrNull(3)) {
+                    "set" -> {
+                        if (message.rolesIdsMentioned.size != 1)
+                            return texts.getString("muterole.invalid").toMessage()
+
+                        transaction { config.muteRole = message.rolesIdsMentioned.first() }
+                        texts.formatString("muterole.set", message.rolesIdsMentioned.first().toRoleMention()).toMessage()
+                    }
+                    "unset" -> {
+                        transaction {
+                            config.muteRole = null
+                        }
+                        texts.getString("muterole.unset").toMessage()
+                    }
+                    else -> texts.getErrorString().toMessage()
+                }
+            }
+            "blacklist" -> {
+                when(message.words.getOrNull(3)) {
+                    "show", "get" -> dslmessage {
+                        TODO()
+                    }
+                    "add" -> {
+                        val cmd = message.words.getOrNull(4)
+                            ?: return texts.getString("blacklist.nocommand").toMessage()
+                        if (transaction { config.blacklistedCommands }.any { it.command.equals(cmd, true) })
+                            return texts.getString("blacklist.add.already").toMessage()
+                        if (CommandList.findCommand(cmd) == null)
+                            return texts.getString("blacklist.add.notfound").toMessage()
+
+                        transaction { BlacklistedCommand.new {
+                            this.command = cmd
+                            this.guild = guild.id
+                        } }
+                        texts.formatString("blacklist.add", cmd).toMessage()
+                    }
+                    "remove", "rm", "delete" -> {
+                        val cmd = message.words.getOrNull(4)
+                            ?: return texts.getString("blacklist.nocommand").toMessage()
+                        val blacklistedCommand = transaction { config.blacklistedCommands }.find { it.command.equals(cmd, true) }
+                            ?: return texts.getString("blacklist.remove.already").toMessage()
+
+                        transaction { blacklistedCommand.delete() }
+                        texts.formatString("blacklist.remove", cmd).toMessage()
+                    }
+                    else -> texts.getErrorString().toMessage()
+                }
+            }
+
             else -> texts.getErrorString().toMessage()
-
         }
-    }
-
-    private fun getGreetingsChannelName(clientStore: ClientStore, config: GuildConfig, texts: CommandLocaleBundle): String {
-        if (config.greetingsChannel.isEmpty()) return texts.getString("greeting.channel.notset")
-
-        val channel = try {
-            clientStore.channels[config.greetingsChannel]
-        } catch (_: Exception) {
-            return texts.getString("greeting.channel.error")
-        }
-
-        return channel.channelId.toChannelMention()
-    }
-
-    private fun extractChannelId(string: String): String {
-        if (string.startsWith('#')) return string.drop(1)
-        if (string.startsWith('<')) return string.drop(2).dropLast(1)
-        throw IllegalArgumentException()
     }
 }
