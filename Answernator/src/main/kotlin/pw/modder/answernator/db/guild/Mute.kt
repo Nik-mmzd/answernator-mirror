15616ea84985dc453f74ba84c05e56cc967ac006
McModder <mcmodder@mcmodder.ru>
2021-07-10 07:23:28 +0300

    remove deprecated dbs

--- Answernator/src/main/kotlin/pw/modder/answernator/db/guild/Mute.kt
+++ Answernator/src/main/kotlin/pw/modder/answernator/db/guild/Mute.kt
@@ -4,10 +4,6 @@ import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IntIdTable
-import org.jetbrains.exposed.sql.Table
-import pw.modder.answernator.db.GuildMutes
-import pw.modder.answernator.db.GuildMutes.autoIncrement
-import pw.modder.answernator.db.GuildMutes.index
 
 class Mute(id: EntityID<Int>): IntEntity(id) {
     companion object : IntEntityClass<Mute>(Mutes)
