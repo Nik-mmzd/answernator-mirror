ca80fe4024e8b9288f1d0da20ecac6ee4a056f48
McModder <mcmodder@mcmodder.ru>
2021-07-17 23:39:24 +0300

    withTyping

--- Answernator/src/main/kotlin/pw/modder/answernator/utils/extensions/bot/CommandsService.kt
+++ Answernator/src/main/kotlin/pw/modder/answernator/utils/extensions/bot/CommandsService.kt
@@ -2,17 +2,14 @@ package pw.modder.answernator.utils.extensions.bot
 
 import dev.kord.common.entity.Permission
 import dev.kord.core.Kord
+import dev.kord.core.behavior.channel.withTyping
 import dev.kord.core.event.message.MessageCreateEvent
 import dev.kord.core.on
-import kotlinx.coroutines.flow.toList
-import kotlinx.coroutines.runBlocking
 import mu.KotlinLogging
-
 import pw.modder.answernator.db.Db
 import pw.modder.answernator.utils.CommandList
 import pw.modder.answernator.utils.Globals
 import pw.modder.answernator.utils.extensions.kord.channelType
-import pw.modder.answernator.utils.extensions.kord.guildId
 import pw.modder.answernator.utils.extensions.kord.reply
 import pw.modder.answernator.utils.extensions.kord.words
 import pw.modder.answernator.utils.locale.LocaleBundle
@@ -23,44 +20,46 @@ suspend fun Kord.commandService() {
     val config = Globals.config
 
     on<MessageCreateEvent> {
-        if (message.content.isEmpty())
-            return@on
-        if (message.author?.isBot != false)
-            return@on
+        message.channel.withTyping {
+            if (message.content.isEmpty())
+                return@on
+            if (message.author?.isBot != false)
+                return@on
 
-        logger.debug { "received message, message text: ${message.content}" }
-        message.data.guildId.value
+            logger.debug { "received message, message text: ${message.content}" }
+            message.data.guildId.value
 
-        val guildConfig = message.data.guildId.value.takeUnless { it == null }?.run { Db.getGuildConfig(asString) }
+            val guildConfig = message.data.guildId.value.takeUnless { it == null }?.run { Db.getGuildConfig(asString) }
 
-        if (message.content.first() != guildConfig?.cmdPrefix ?: config.prefix)
-            return@on
+            if (message.content.first() != guildConfig?.cmdPrefix ?: config.prefix)
+                return@on
 
-        val texts = LocaleBundle("botGlobal", guildConfig?.lang ?: config.lang)
+            val texts = LocaleBundle("botGlobal", guildConfig?.lang ?: config.lang)
 
-        val words = message.words
-        val command = CommandList.findCommand(name = words.first().drop(1), channelType = message.channelType)
-            ?: return@on // if command not found: do nothing
+            val words = message.words
+            val command = CommandList.findCommand(name = words.first().drop(1), channelType = message.channelType)
+                ?: return@on // if command not found: do nothing
 
-        if (guildConfig != null && Db.isBlackListed(guildConfig.guildId, command.name)) {
-            if (message.author?.id != message.getGuild().owner.id
-                && message.getAuthorAsMember()?.getPermissions()?.contains(Permission.Administrator) != true)
+            if (guildConfig != null && Db.isBlackListed(guildConfig.guildId, command.name)) {
+                if (message.author?.id != message.getGuild().owner.id
+                    && member?.getPermissions()?.contains(Permission.Administrator) != true)
                     return@on
-        }
+            }
 
-        logger.debug { "found command ${command.name}, checking" }
-        if (!command.check(message, texts.locale)) {
-            message.reply(texts.getString("bot.noPerms"))
-            return@on
-        }
+            logger.debug { "found command ${command.name}, checking" }
+            if (!command.check(message, texts.locale)) {
+                message.reply(texts.getString("bot.noPerms"))
+                return@on
+            }
 
-        logger.debug { "found command ${command.name}, running" }
-        try {
-            command.action(message, words.drop(1).filter { it.isNotEmpty() }, texts.locale)
+            logger.debug { "found command ${command.name}, running" }
+            try {
+                command.action(message, words.drop(1).filter { it.isNotEmpty() }, texts.locale)
 
-        } catch (e: Exception) { // and any other exception
-            logger.error(e) { "got error while running command" }
-            message.reply(texts.formatString("bot.error", "${config.prefix}${command.name}"))
+            } catch (e: Exception) { // and any other exception
+                logger.error(e) { "got error while running command" }
+                message.reply(texts.formatString("bot.error", "${config.prefix}${command.name}"))
+            }
         }
     }
 }
