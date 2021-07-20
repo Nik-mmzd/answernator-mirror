e7868b6c680e8b99f3d695defb253bdd07b52265
McModder <mcmodder@mcmodder.ru>
2021-07-20 22:52:13 +0300

    fix clear command

--- tools/src/main/kotlin/pw/modder/answernator/tools/commands/localized/Clear.kt
+++ tools/src/main/kotlin/pw/modder/answernator/tools/commands/localized/Clear.kt
@@ -2,9 +2,10 @@ package pw.modder.answernator.tools.commands.localized
 
 import dev.kord.common.entity.Permission
 import dev.kord.common.entity.Snowflake
-import dev.kord.core.behavior.channel.TextChannelBehavior
+import dev.kord.core.behavior.getChannelOf
 import dev.kord.core.entity.Guild
 import dev.kord.core.entity.Message
+import dev.kord.core.entity.channel.TextChannel
 import kotlinx.coroutines.flow.toList
 import pw.modder.answernator.db.guild.Config
 import pw.modder.answernator.utils.Command
@@ -53,7 +54,7 @@ class Clear: LocalizedGuildCommand {
         if (messages.size == 1)
             message.channel.deleteMessage(messages.first())
         else
-            (guild.channelBehaviors.first { it.id == message.channelId } as TextChannelBehavior).bulkDelete(messages.toSet())
+            guild.getChannelOf<TextChannel>(message.channelId).bulkDelete(messages.toSet())
 
         message.reply(texts.formatString("done", messages.size))
     }
